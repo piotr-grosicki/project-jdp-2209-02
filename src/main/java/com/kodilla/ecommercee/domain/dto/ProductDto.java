5d16918d10dba230d2a5f1d92a748882cc5387f1
Michal Czaplinski <56223086+s20308-pj@users.noreply.github.com>
2022-08-21 20:08:06 +0200

    added: ProductDto, Group;  corrected: ProductController

--- src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/ProductDto.java
@@ -1,5 +1,6 @@
 package com.kodilla.ecommercee.domain.dto;
 
+import com.kodilla.ecommercee.domain.Group;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,5 @@ public class ProductDto {
     private String name;
     private String description;
     private float price;
+    private Group group;
 }
