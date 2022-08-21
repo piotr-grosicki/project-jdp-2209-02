05f22126a7b17fa7f690ff0269fde0c4e46bca7e
s20308-pj <56223086+s20308-pj@users.noreply.github.com>
2022-08-21 15:04:11 +0200

    added: ProductController; inserted: controller calss in to controller package

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,4 +1,31 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("v1/shop/products")
+@AllArgsConstructor
 public class ProductController {
+    @GetMapping
+    public ResponseEntity<Void> getProducts() {
+        System.out.println("get all products");
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("{productId}")
+    public ResponseEntity<Void> getProduct(@PathVariable Long productId) {
+        System.out.println("get one product with id=" + productId);
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping
+    public ResponseEntity<Void> addNewProduct(){
+        System.out.println("Adding new product");
+        return ResponseEntity.ok().build();
+    }
+    @PutMapping
+    public ResponseEntity<Void> updateProduct(){
+        System.out.println("updating product");
+        return ResponseEntity.ok().build();
+    }
 }
