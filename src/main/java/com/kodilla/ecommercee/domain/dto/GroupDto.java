550c0d802c1205d65a671c7160382a0d5f7a0c19
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-08-22 17:16:52 +0200

    JDP220902-1 fix - GroupDto

--- src/main/java/com/kodilla/ecommercee/domain/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/GroupDto.java
@@ -1,5 +1,12 @@
 package com.kodilla.ecommercee.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GroupDto {
     private Long id;
     private String name;
