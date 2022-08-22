2ff7411406d05d51b625d4055acc1b9816d37d25
Piotr Tynek <tynek.piotr@gmail.com>
2022-08-22 21:06:00 +0200

    Fixed problems JDP220902-4

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,8 +1,9 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.domain.CartDto;
-import com.kodilla.ecommercee.domain.OrderDto;
+import com.kodilla.ecommercee.domain.dto.CartDto;
+import com.kodilla.ecommercee.domain.dto.OrderDto;
 import com.kodilla.ecommercee.domain.UserDto;
+import com.kodilla.ecommercee.domain.dto.ProductDto;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,8 +24,11 @@ public class OrderController {
     @GetMapping(value = "{orderId}")
     public OrderDto getOrder(@PathVariable int orderId){
         UserDto userDto = new UserDto(1L, "Login1", "mail@mail.com", "Adress 1", false, 123L);
-        CartDto cartDto = new CartDto(1L, userDto.getId(), 1L, new ArrayList<>());
-        return new OrderDto(1L, userDto, cartDto, false, new BigDecimal(22.3));
+        CartDto cartDto = new CartDto(1L, 1L, 1L, new ArrayList<>());
+        List<ProductDto> productDtoList = new ArrayList<>();
+        ProductDto productDto = new ProductDto(1L,"productName","productDescription",1000);
+        cartDto.getProductDtoList().add(productDto);
+        return new OrderDto(1L, userDto, cartDto, false, new BigDecimal(22.3), productDtoList);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -36,7 +40,10 @@ public class OrderController {
     public OrderDto updateOrder(@RequestBody OrderDto orderDto){
         UserDto userDto = new UserDto(1L, "Login15", "mail@mail.com", "Adress 1", false, 123L);
         CartDto cartDto = new CartDto(1L, userDto.getId(), 1L, new ArrayList<>());
-        return new OrderDto(1L, userDto, cartDto, true, new BigDecimal(25.0));
+        List<ProductDto> productDtoList = new ArrayList<>();
+        ProductDto productDto = new ProductDto(1L,"productName","productDescription",1000);
+        cartDto.getProductDtoList().add(productDto);
+        return new OrderDto(1L, userDto, cartDto, true, new BigDecimal(25.0), productDtoList);
 
     }
 }
--- src/main/java/com/kodilla/ecommercee/domain/dto/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/OrderDto.java
@@ -1,9 +1,11 @@
 package com.kodilla.ecommercee.domain.dto;
 
+import com.kodilla.ecommercee.domain.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -12,10 +14,20 @@ import java.util.List;
 @NoArgsConstructor
 public class OrderDto {
     private long id;
-    private long userId;
-    private long cartId;
+    private UserDto userDto;
+    private CartDto cartDto;
     private LocalDate orderDate;
     private boolean isPaid;
-    private double totalPrice;
+    private BigDecimal totalPrice;
     private List<ProductDto> productDtoList;
+
+    public OrderDto(long id, UserDto userDto, CartDto cartDto,  boolean isPaid, BigDecimal totalPrice, List<ProductDto> productDtoList) {
+        this.id = id;
+        this.userDto = userDto;
+        this.cartDto = cartDto;
+        this.orderDate = LocalDate.now();
+        this.isPaid = isPaid;
+        this.totalPrice = totalPrice;
+        this.productDtoList = productDtoList;
+    }
 }
