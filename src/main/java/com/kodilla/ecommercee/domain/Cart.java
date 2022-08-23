2d91e8137279664abdcd4f02f587f4cb523e045e
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-08-23 19:16:48 +0200

    JDP220902-12 - implementacja encji Product

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,26 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "CARTS")
+
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    private long id;
+
+    @ManyToMany(cascade = CascadeType.ALL,mappedBy = "carts")
+    private List<Product> products;
+}
