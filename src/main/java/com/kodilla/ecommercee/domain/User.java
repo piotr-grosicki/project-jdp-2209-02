4e81ddc16e4af5f21dc749d4a95b466187b69a32
Michal Czaplinski <56223086+s20308-pj@users.noreply.github.com>
2022-08-24 16:04:10 +0200

    JDP220902-15 4 adding: to User entity variable userKey, and lastLogin

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -6,7 +6,9 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -18,14 +20,22 @@ public class User {
     @NotNull
     @Column(name = "ID", unique = true)
     private Long id;
+    @NotNull
     @Column(name = "LOGIN")
     private String login;
+    @NotNull
     @Column(name = "mail")
     private String mail;
+    @NotNull
     @Column(name = "ADDRESS")
     private String address;
+    @NotNull
     @Column(name = "IS_BLOCKED")
     private boolean isBlocked;
+    @Column(name = "USER_KEY")
+    private UUID userKey;
+    @Column(name = "LAST_LOGIN")
+    private LocalTime lastLogin;
     @OneToMany(
             targetEntity = Order.class,
             mappedBy = "user",
@@ -33,8 +43,7 @@ public class User {
             fetch = FetchType.LAZY
     )
     private List<Order> orders;
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "CART_ID")
+    @OneToOne
     private Cart cart;
 }
 
