443c1d6bcf5c8d3a4ef1eda6bc3e7bdf9abd027c
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-08-25 18:18:31 +0200

    Cart tests

---
+++ src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee.domain;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CartTestSuite {
+
+
+
+}
