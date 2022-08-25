7a95ea3fa70df737c0b94800d379e2fa776f69d5
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-08-25 21:23:27 +0200

    JDP220902-19 testy encji Carts

--- src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -16,12 +18,40 @@ class CartTestSuite {
     @Test
     void addCartTest() {
         // Given
-        User user = new User();
-        Order order = new Order();
         Cart cart = new Cart();
 
-        cart.setUser(user);
-        cart.setOrder(order);
+        //When
+        cartRepository.save(cart);
+
+        //Then
+        assertEquals(1, cartRepository.findAll().size());
+        assertTrue(cartRepository.findById(cart.getId()).isPresent());
+
+        //CleanUp
+        cartRepository.deleteById(cart.getId());
+    }
+
+    @Test
+    void getAllCarts() {
+        //Given
+        Cart cart1 = new Cart();
+        Cart cart2 = new Cart();
+
+        //When
+        cartRepository.save(cart1);
+        cartRepository.save(cart2);
+
+        //Then
+        assertEquals(2, cartRepository.findAll().size());
+
+        //CleanUp
+        cartRepository.deleteAll();
+    }
+
+    @Test
+    void getCartById() {
+        //Given
+        Cart cart = new Cart();
 
         //When
         cartRepository.save(cart);
@@ -30,6 +60,21 @@ class CartTestSuite {
         assertTrue(cartRepository.findById(cart.getId()).isPresent());
 
         //CleanUp
-        cartRepository.deleteById(cart.getId());
+        cartRepository.deleteAll();
+    }
+
+    @Test
+    void deleteCart() {
+        //Given
+        Cart cart = new Cart();
+
+        //When
+        cartRepository.save(cart);
+        Long id = cart.getId();
+        cartRepository.deleteById(id);
+
+        //Then
+        assertEquals(Optional.empty(), cartRepository.findById(id));
+        assertEquals(0, cartRepository.findAll().size());
     }
 }
