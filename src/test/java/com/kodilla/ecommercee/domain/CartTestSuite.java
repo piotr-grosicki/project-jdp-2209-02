8a97f2db6b6f235f036d7e39d3ebab6c855e4881
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-08-25 19:03:17 +0200

    addCart

--- src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/CartTestSuite.java
@@ -1,5 +1,8 @@
 package com.kodilla.ecommercee.domain;
 
+import com.kodilla.ecommercee.repository.CartRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +10,26 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CartTestSuite {
 
+    @Autowired
+    private CartRepository cartRepository;
 
+    @Test
+    void addCartTest() {
+        // Given
+        User user = new User();
+        Order order = new Order();
+        Cart cart = new Cart();
 
+        cart.setUser(user);
+        cart.setOrder(order);
+
+        //When
+        cartRepository.save(cart);
+
+        //Then
+        assertTrue(cartRepository.findById(cart.getId()).isPresent());
+
+        //CleanUp
+        cartRepository.deleteById(cart.getId());
+    }
 }
