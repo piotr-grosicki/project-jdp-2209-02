87ab03c003aab1349a3b11da4e07383fa087f3e7
SzymBurz <szb210@gmail.com>
2022-08-25 15:10:29 +0200

    Order class

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,2 +1,37 @@
-package com.kodilla.ecommercee.domain;public class Order {
+package com.kodilla.ecommercee.domain;
+
+import com.kodilla.ecommercee.domain.dto.ProductDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Entity(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "userId")
+    private long userId;
+
+    @Column(name = "cartId")
+    private long cartId;
+
+    @Column(name = "orderDate")
+    private LocalDate orderDate;
+
+    @Column(name = "isPaid")
+    private boolean isPaid;
+
+    @Column(name = "totalPrice")
+    private BigDecimal totalPrice;
+
 }
