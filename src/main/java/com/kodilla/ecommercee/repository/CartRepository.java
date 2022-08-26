875088815d2652b7d0f7a7cb0686505def2e6709
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-08-26 18:12:58 +0200

    buildFix 26.08.2022

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -4,6 +4,9 @@ import com.kodilla.ecommercee.domain.Cart;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @Repository
 public interface CartRepository extends CrudRepository<Cart, Long> {
 
--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @Repository
 public interface UserRepository extends CrudRepository<User,Long> {
 }
