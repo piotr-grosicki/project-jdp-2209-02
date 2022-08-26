2e2478e32afef18cd236ffc45a6afc17756d37a3
Piotr Tynek <tynek.piotr@gmail.com>
2022-08-26 18:07:41 +0200

    add transactional annotation to repositories

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -4,6 +4,9 @@ import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
