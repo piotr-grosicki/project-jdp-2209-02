f4d427729f1ff67fcaa1a771382fa16bedc0c9df
krzysiekn111 <krzysiekn111@gmail.com>
2022-08-26 11:59:55 +0200

    JDP220902-23

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -1,10 +1,23 @@
 package com.kodilla.ecommercee.repository;
 
 import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends CrudRepository<Cart, Long> {
+
+    @Override
+    List<Cart> findAll();
+
+    @Override
+    Optional<Cart> findById(Long longId);
+
+    @Override
+    Cart save(Cart cart);
 
 }
