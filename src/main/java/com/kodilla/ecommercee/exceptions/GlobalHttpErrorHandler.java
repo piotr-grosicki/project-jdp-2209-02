e4494400b55b563821354ec01646ce4052d9d1c2
Piotr Tynek <tynek.piotr@gmail.com>
2022-08-29 21:59:43 +0200

    Fixed building project

--- src/main/java/com/kodilla/ecommercee/exceptions/GlobalHttpErrorHandler.java
+++ src/main/java/com/kodilla/ecommercee/exceptions/GlobalHttpErrorHandler.java
@@ -2,10 +2,12 @@ package com.kodilla.ecommercee.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Component("myBean")
 @ControllerAdvice
 public class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
 
