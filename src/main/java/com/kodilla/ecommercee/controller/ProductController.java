d4ab4f0fec5a06349359d2fa970e633c640a16c9
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-08-30 16:30:55 +0200

    added exception in DeleteMapping

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.kodilla.ecommercee.domain.dto.ProductDto;
 import com.kodilla.ecommercee.exceptions.GroupNotFoundException;
 import com.kodilla.ecommercee.exceptions.ProductNotFoundException;
 import com.kodilla.ecommercee.mapper.ProductMapper;
+import com.kodilla.ecommercee.repository.ProductRepository;
 import com.kodilla.ecommercee.service.ProductDbService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -18,37 +19,43 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
 
-    private final ProductDbService service;
+    private final ProductDbService productService;
     private final ProductMapper productMapper;
+    private final ProductRepository productRepository;
 
     @GetMapping
     public ResponseEntity<List<ProductDto>> getProducts() {
-        List<Product> products = service.getAllProducts();
+        List<Product> products = productService.getAllProducts();
         return ResponseEntity.ok(productMapper.mapToProductDtoList(products));
     }
 
     @GetMapping(value = "{productId}")
     public ResponseEntity<ProductDto> getProduct(@PathVariable Long productId) throws ProductNotFoundException {
-       return ResponseEntity.ok(productMapper.mapToProductDto(service.getProductById(productId)));
+       return ResponseEntity.ok(productMapper.mapToProductDto(productService.getProductById(productId)));
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> addNewProduct(@RequestBody ProductDto productDto) throws GroupNotFoundException {
         Product product = productMapper.mapToProduct(productDto);
-        service.saveProduct(product);
+        productService.saveProduct(product);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ProductDto> updateProduct(@RequestBody ProductDto productDto) throws GroupNotFoundException {
         Product product = productMapper.mapToProduct(productDto);
-        Product updatedProduct = service.saveProduct(product);
+        Product updatedProduct = productService.saveProduct(product);
         return ResponseEntity.ok(productMapper.mapToProductDto(updatedProduct));
     }
 
     @DeleteMapping(value = "{productId}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
-        service.deleteProduct(productId);
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) throws ProductNotFoundException{
+
+        if(productRepository.existsById(productId)) {
+            productService.deleteProduct(productId);
+        } else {
+            throw new ProductNotFoundException();
+        }
         return ResponseEntity.ok().build();
     }
 }
